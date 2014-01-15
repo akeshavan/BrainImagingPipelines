7071a4188cf18eb982c99a2d22fdeb1515c27b61
Anisha Keshavan <akeshavan@ucla.edu>
2014-01-14 16:19:46 -0800

    enh/fix/wip: nicer subs

--- bips/workflows/nelsonlab/wips/fmri_7T.py
+++ bips/workflows/nelsonlab/wips/fmri_7T.py
@@ -134,7 +134,7 @@ def get_substitutions(subject_id):
         subs.append(('_threshold%d/'%i, '%s_r%02d_'%(subject_id, i)))
         subs.append(("_create_nuisance_filter%d/"%i,'%s_r%02d_'%(subject_id, i)))
         subs.append(("_bandpass_filter%d/"%i,'%s_r%02d_'%(subject_id, i)))
-        subs.append(("_applymask0/"%i,'%s_r%02d_'%(subject_id, i)))
+        subs.append(("_applymask%d/"%i,'%s_r%02d_'%(subject_id, i)))
 
     return subs
 
