ec6ca7a2b9450e8e5f02cf2ce7582f92cc47cfea
akeshavan <keshavan@berkeley.edu>
2014-02-21 12:25:11 -0800

    fix: added init

--- bips/workflows/__init__.py
+++ bips/workflows/__init__.py
@@ -11,4 +11,4 @@ from .gablab.wips.fmri.resting import wip_resting_correlation_QA, map_correlatio
 from .gablab.wips.fmri.viz import synced_corr_display_h5
 from .gablab.wips.smri import test_freesurfer, normalize_structural, normalize_functionals, kelly_kapowski, freesurfer_brain_masks, wip_divide_parcellations
 from .gablab.wips.utils import take_mean_image, plot_contours, change_datatype
-from .nelsonlab.wips import fmri_7T, fsl_multiple_regression, fsl_one_sample_t_test
+from .nelsonlab.wips import fmri_7T, fsl_multiple_regression, fsl_one_sample_t_test, seed_based_connectivity2, fmri_7T_multiband
